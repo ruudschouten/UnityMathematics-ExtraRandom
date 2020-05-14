36e4846d8bc63bdb38585fa135f5a8d5962144ca
ruudschouten <ruud.schouten@outlook.com>
2020-05-14 12:02:32 +0200

    Interface which all Randoms will implement

---
+++ ExtraRandom/Interface/IRandom.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtraRandom.Interface
+{
+    public interface IRandom
+    {
+        int NextInt();
+        int NextInt(int max);
+        int NextInt(int min, int max);
+        float NextFloat();
+        float NextFloat(float max);
+        float NextFloat(float min, float max);
+    }
+}
