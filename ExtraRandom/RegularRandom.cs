02a8cc4ea730e75efc42669975bbaa4b533c42b1
ruudschouten <ruud.schouten@outlook.com>
2020-05-14 12:03:01 +0200

    Implemented Regular Random behavior

---
+++ ExtraRandom/RegularRandom.cs
@@ -0,0 +1,23 @@
+﻿namespace ExtraRandom
+{
+    public class RegularRandom : BaseRandom
+    {
+        public RegularRandom() : base()
+        {
+        }
+        
+        public RegularRandom(uint seed) : base(seed)
+        {
+        }
+        
+        public override int NextInt(int min, int max)
+        {
+            return Random.NextInt(min, max);
+        }
+
+        public override float NextFloat(float min, float max)
+        {
+            return Random.NextFloat(min, max);
+        }
+    }
+}
